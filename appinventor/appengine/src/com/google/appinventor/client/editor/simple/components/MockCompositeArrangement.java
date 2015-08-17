966d8740032f0662100e1a8185abfa7edfb72bb0
E-H <e.hon.dev@gmail.com>
2015-08-18 06:31:38 +0800

    Implement to build composition

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockCompositeArrangement.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/components/MockCompositeArrangement.java
@@ -6,25 +6,105 @@
 
 package com.google.appinventor.client.editor.simple.components;
 
+import com.google.appinventor.client.Ode;
+import com.google.appinventor.client.OdeAsyncCallback;
 import com.google.appinventor.client.editor.simple.SimpleEditor;
+import com.google.appinventor.client.editor.youngandroid.YaFormEditor;
+import com.google.appinventor.client.editor.youngandroid.properties.YoungAndroidHorizontalAlignmentChoicePropertyEditor;
+import com.google.appinventor.client.editor.youngandroid.properties.YoungAndroidVerticalAlignmentChoicePropertyEditor;
+import com.google.appinventor.client.output.OdeLog;
+import com.google.appinventor.client.properties.json.ClientJsonParser;
 import com.google.appinventor.components.common.ComponentConstants;
+import com.google.appinventor.shared.properties.json.JSONArray;
+import com.google.appinventor.shared.properties.json.JSONObject;
+import com.google.appinventor.shared.properties.json.JSONValue;
+import com.google.appinventor.shared.rpc.project.ChecksumedFileException;
+import com.google.appinventor.shared.rpc.project.ChecksumedLoadFile;
+import com.google.appinventor.shared.youngandroid.YoungAndroidSourceAnalyzer;
 
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.TreeItem;
 
+/**
+ * It is basically a MockVerticalArrangement but with special configurations.
+ */
 public final class MockCompositeArrangement extends MockHVArrangement {
 
-  public static final String TYPE = "CompositeArrangement";
+  private String type;
+  private String name;
+
+  private static final String H_A = PROPERTY_NAME_HORIZONTAL_ALIGNMENT;
+  private static final String V_A = PROPERTY_NAME_VERTICAL_ALIGNMENT;
+  private static final String H_A_DEFAULT =
+      ComponentConstants.HORIZONTAL_ALIGNMENT_DEFAULT + "";
+  private static final String V_A_DEFAULT =
+      ComponentConstants.VERTICAL_ALIGNMENT_DEFAULT + "";
 
   /**
    * Creates a new MockCompositeArrangement component.
-   * It is basically a MockVerticalArrangement but with special configurations.
-   * It uses extension.png as the image so that users view it as an extension.
    *
    * @param editor  editor of source file the component belongs to
+   * @param type  the type this MockCompositeArrangement represents
+   * @param name  the displayed name of the type
    */
-  public MockCompositeArrangement(SimpleEditor editor) {
-    super(editor, TYPE, images.extension(),
+  public MockCompositeArrangement(SimpleEditor editor, String type, String name) {
+    super(editor, name, images.extension(),
         ComponentConstants.LAYOUT_ORIENTATION_VERTICAL);
+
+    this.type = type;
+    this.name = name;
+
+    addProperty(H_A, H_A_DEFAULT, H_A,
+        new YoungAndroidHorizontalAlignmentChoicePropertyEditor());
+    addProperty(V_A, V_A_DEFAULT, V_A,
+        new YoungAndroidVerticalAlignmentChoicePropertyEditor());
+  }
+
+  public void onCreateFromPalette() {
+    Ode ode = Ode.getInstance();
+    long projectId = ode.getCurrentYoungAndroidProjectId();
+
+    String scmPath = "assets/external_comps/" + type + "/composition.scmx";
+    ode.getProjectService().load2(projectId, scmPath,
+        new OdeAsyncCallback<ChecksumedLoadFile>() {
+          @Override
+          public void onSuccess(ChecksumedLoadFile result) {
+            String content;
+            try {
+              content = result.getContent();
+            } catch (ChecksumedFileException e) {
+              onFailure(e);
+              return;
+            }
+
+            if (getContainer() == null) {
+              // this handles the weird situation that this method is called
+              // twice when it is dropped to the screen that in the first call,
+              // getContainer() yields null
+              return;
+            }
+
+            JSONObject props = YoungAndroidSourceAnalyzer.parseSourceFile(
+                content, new ClientJsonParser()).get("Properties").asObject();
+
+            JSONValue hAValue = props.get(H_A);
+            JSONValue vAValue = props.get(V_A);
+
+            if (hAValue != null) {
+              changeProperty(H_A, hAValue.asString().getString());
+            }
+
+            if (vAValue != null) {
+              changeProperty(V_A, vAValue.asString().getString());
+            }
+
+            JSONArray components = props.get("$Components").asArray();
+            for (JSONValue component : components.getElements()) {
+              ((YaFormEditor) editor).createMockComponent(component.asObject(),
+                  MockCompositeArrangement.this);
+            }
+          }
+        });
   }
 
   @Override
@@ -39,8 +119,8 @@ public final class MockCompositeArrangement extends MockHVArrangement {
     // hide all properties except Visible
     if (propertyName.equals(PROPERTY_NAME_WIDTH) ||
         propertyName.equals(PROPERTY_NAME_HEIGHT) ||
-        propertyName.equals(PROPERTY_NAME_HORIZONTAL_ALIGNMENT) ||
-        propertyName.equals(PROPERTY_NAME_VERTICAL_ALIGNMENT)) {
+        propertyName.equals(H_A) ||
+        propertyName.equals(V_A)) {
       return false;
     }
 
@@ -50,6 +130,11 @@ public final class MockCompositeArrangement extends MockHVArrangement {
   @Override
   public boolean acceptableTarget() {
     return false;
+  }
+
+  @Override
+  public String getVisibleTypeName() {
+    return name;
   }
 
   @Override
